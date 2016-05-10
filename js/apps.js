05858e83ebaea77b8379105bd63827ead734a288
btchae <bchae@nyu.edu>
2016-05-10 12:26:11 -0400

    First commit

---
+++ js/apps.js
@@ -0,0 +1,14 @@
+
+/////My Columns of connect-foar////
+var gameField = new Array();
+var makeBoard =function(){
+  gameField = new Array();
+  for(var i=0; i<6; i++){
+    gameField[i] = new Array();
+    for(var j=0; j<7; j++){
+      gameField[i].push(0);
+    }
+  }
+}
+makeBoard();
+console.log(gameField);
