208ce92a57a843d9595cae304be244f5e62c38b2
btchae <bchae@nyu.edu>
2016-05-11 09:07:42 -0400

    starting my morning for commiting

--- js/apps.js
+++ js/apps.js
@@ -3,44 +3,70 @@
 var emptySpace = 0;
 var playerSpace = 1;
 var otherPlayerSpace = 2;
-
-var gameBoard = new Array();
-var makeBoard =function(){
-  gameBoard = new Array();
-  for(var i=0; i<6; i++){
+////for now makeBoard has j,i but will eventually be 0
+var gameBoard = [];
+var makeBoard = function(){
+  gameBoard = [];
+  for(var i = 0; i < 6; i++){
     gameBoard[i] = new Array();
-    for(var j=0; j<7; j++){
+    for(var j = 0; j < 7; j++){
       gameBoard[i].push(0);
     }
   }
 }
+//gameBoard x and y axis was very confusing for a bit. To make things clear. rows = y axis, columns = x axis. gameBoard[row][column]
+//gameBoard[y][x]
 
 makeBoard();
 console.log(gameBoard);
 console.log(gameBoard[0]);
-console.log(gameBoard[0][0]);
-var playerMove = function() {
-	.addEventListener("click", )
-};
+console.log(gameBoard[0][1]);
+// var playerMove = function() {
+// 	.addEventListener("click", )
+// };
 
 var checkWin = function() {
-	if (tile >= 7) {
 
-	}
 };
+
+
 
 var boardValue = function(row, col) {
 	 if(gameBoard[row] == undefined || gameBoard[row][col] == undefined){
-    return -1;
+    return undefined;
   } else {
     return gameBoard[row][col];
   }
-}
-}
+};
 
+var checkRowSpace = function(col) {
+	  for(var i = 0; i < 6; i++){
+	  	// console.log(gameBoard[i][col]);
+	  	// console.log(boardValue(i, col));
+    	if (gameBoard[i][col] === 0 && gameBoard[i+1][col] === 0) {
+    		break;
+    	} else {
+    		// gameBoard[i][col] = 1; I was really confused about why I wasn't getting a coordinate pair
+    		//but then I figured it out as indicated below.
+    		gameBoard[i][col] += 1;
+    		console.log(gameBoard);
+    	}
+}
+};
+///Just realized the above checkRowSpace function isn't working because right now my gameBoard[i][j] is not an integer
 var resetGame = function() {
 
 };
+
+checkRowSpace(0);
+checkRowSpace(0);
+
+///these console logs were to help me understand/comprehend that whole row vs. column confusion I had
+// console.log(boardValue(2, 3));
+// console.log(boardValue(3, 2));
+// console.log(boardValue(5,6));
+///////////////////////////////////////
+
 ////Pseudo code
 ////Make the game board
 ////game board will be a series of arrays
